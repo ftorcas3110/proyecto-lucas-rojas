9ef5cdb9e03daa0b6e2a48a09392026c9defe7cc
ftorcas3110 <borjatorres1228@gmail.com>
2024-04-12 13:52:53 +0200

    asd123

--- src/components/button-form.js
+++ src/components/button-form.js
@@ -1,20 +1,20 @@
 "use client";
 import { useFormStatus } from "react-dom";
-import toast, { Toaster, Bounce } from "react-hot-toast";
+// import toast, { Toaster, Bounce } from "react-hot-toast";
 
 function Button({ title }) {
   const { pending } = useFormStatus();
-  const notify = () => toast.success('Ganamos', {
-    position: "bottom-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-    transition: Bounce,
-  });
+  // const notify = () => toast.success('Ganamos', {
+  //   position: "bottom-right",
+  //   autoClose: 5000,
+  //   hideProgressBar: false,
+  //   closeOnClick: true,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   progress: undefined,
+  //   theme: "light",
+  //   transition: Bounce,
+  // });
 
   return (
     <div className="mx-auto text-center">
@@ -22,11 +22,11 @@ function Button({ title }) {
         type="submit"
         disabled={pending}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center"
-        onClick={notify}
+        // onClick={notify}
       >
         {title}
       </button>
-      <Toaster />
+      {/* <Toaster /> */}
     </div>
   );
 }
