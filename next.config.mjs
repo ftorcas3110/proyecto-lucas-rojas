6b2e874f7f8cd1df5f7dcad27b75e9b5e9de6939
ftorcas3110 <borjatorres1228@gmail.com>
2024-05-13 12:24:09 +0200

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Otras configuraciones...
+  
+    // Configuración para aumentar el tiempo límite de generación de páginas estáticas
+    staticPageGenerationTimeout: 120000, // Tiempo en milisegundos (en este caso, 2 minutos)
+  };
+  
+  export default nextConfig;
+  
