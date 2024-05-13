b040050c13717f1b4971868d07fd8cac03e6e1f7
ftorcas3110 <borjatorres1228@gmail.com>
2024-05-13 14:26:05 +0200

    a

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = globalThis.prisma || new PrismaClient()
+let prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
